a5a7b0730c10264dd86cabe9c3ca09cb184e505d
zdz <1696498456@qq.com>
2016-09-21 23:44:41 +0800

    ~

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
         path: __dirname + '/dist',
         filename: 'app.js',
         chunkFilename: '[name].[chunkhash:8].js',
-        publicPath: '/dist/'
+        publicPath: './dist/'
     },
     module: {
         loaders: [{
