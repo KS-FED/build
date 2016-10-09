bfd1cbd492307913276e5db8ca07a63b46c4d4f3
刘丽辉 <liulihui@kashuo.com>
2016-10-09 14:21:45 +0800

    tab导航

--- dev/js/views/home/index.vue
+++ dev/js/views/home/index.vue
@@ -1,7 +1,7 @@
 <template>
-    <div>
+    <!-- <div>
         <pre>
-        <code class="javascript">
+        <code class="html">
             var a = 'sss';
             console.log(a)
         </code>
@@ -16,6 +16,31 @@
             }
         </code>
         </pre>
+    </div> -->
+    <div style="padding:20px 100px;">
+        <br>
+        <!-- <a class="h3" v-link="{name:'button'}">button（按钮）</a><br>
+        <a class="h3" v-link="{name:'layout'}">layout</a><br>
+        <a class="h3" v-link="{name:'form'}">form</a><br>
+        <a class="h3" v-link="{name:'components'}">components</a><br>
+        <a class="h3" v-link="{name:'icons'}">icons</a><br>
+        <a class="h3" v-link="{name:'svg'}">svg</a><br>
+        <a class="h3" v-link="{name:'collapse'}">collapse</a><br>
+        <a class="h3" v-link="{name:'dropdown'}">dropdown</a><br>
+        <a class="h3" v-link="{name:'validatorradio'}">validatorradio</a><br>
+        <a class="h3" v-link="{name:'validatorex1'}">validatorex1</a><br>
+        <a class="h3" v-link="{name:'form-unit'}">form-unit</a><br>
+        <a class="h3" v-link="{name:'table2'}">table2</a><br>
+        <a class="h3" v-link="{name:'search-condition'}">search-condition</a><br> -->
+        <a class="h3" v-link="{name:'tab-bor'}">tab-bor（tab导航——边框）</a><br>
+        <!-- <a class="h3" v-link="{name:'tab-bg'}">tab-bg（tab导航——背景）</a><br>
+        <a class="h3" v-link="{name:'rules-introduce'}">rules-introduce</a><br>
+        <a class="h3" v-link="{name:'upload-input'}">upload-input</a><br>
+        <a class="h3" v-link="{name:'paging'}">paging(分页)</a><br>
+        <a class="h3" v-link="{name:'dialog-content'}">dialog-content(内容弹出框)</a><br>
+        <a class="h3" v-link="{name:'toast'}">toast(提示框)</a><br>
+        <a class="h3" v-link="{name:'date'}">date(日期)</a><br>
+        <a class="h3" v-link="{name:'step'}">step(步骤)</a><br> -->
     </div>
 </template>
 <script >
