1267367147e2be6f0dc3307befb69053f44bff00
zdz <1696498456@qq.com>
2016-10-09 14:19:16 +0800

    ~

--- dev/js/views/home/index.vue
+++ dev/js/views/home/index.vue
@@ -7,15 +7,20 @@
         </code>
         </pre>
         <pre>
-        <code class="scss">
-            %tb {
-                display:table;
-            }
-            .tb {
-                @extend %tb;
-            }
+        <code class="javascript">
+            var a = 'sss';
+            console.log(a)
         </code>
         </pre>
+
+          <pre>
+        <code class="javascript">
+            var a = 'sss';
+            console.log(a)
+        </code>
+        </pre>
+       
+
     </div>
 </template>
 <script >
