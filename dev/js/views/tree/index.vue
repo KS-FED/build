ab8c8094d73b7b194032fb0699730ae1006c1fd9
zdz <1696498456@qq.com>
2016-12-19 17:24:09 +0800

    ~

---
+++ dev/js/views/tree/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <h2>tree</h2>
+    </div>
+</template>
+<script type="text/javascript">
+    export default {
+        data (){
+            return {}
+        },
+        ready (){
+        }
+    }
+</script>
